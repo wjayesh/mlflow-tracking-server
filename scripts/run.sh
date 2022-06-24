e834efb3e5d32f3011aae368e19e758002deca22
Jayesh Sharma <wjayesh@outlook.com>
2022-06-24 11:35:52 +0530

    add option to implement in proxied access mode

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,19 +2,22 @@
 
 metadata_store=''
 artifact_store=''
+serve_artifacts=''
 
 print_usage() {
   printf "Usage: run.sh [options]\n"
     printf "Options:\n"
-    printf "  -h, --help\t\t\tPrints this help message\n"
-    printf "  -m, --metadata-store <path>\tPath to the metadata store\n"
-    printf "  -a, --artifact-store <path>\tPath to the artifact store\n"
+    printf "  -h, \t\t\tPrints this help message\n"
+    printf "  -m,  <path>\tPath to the metadata store\n"
+    printf "  -a,  <path>\tPath to the artifact store\n"
+    printf "  -s,  \tServe artifacts (proxied access)\n"
 }
 
-while getopts 'm:a:' arg; do
+while getopts 'hm:a:s' arg; do
   case "${arg}" in
     m) metadata_store="${OPTARG}" ;;
     a) artifact_store="${OPTARG}" ;;
+    s) serve_artifacts="True" ;;
     h) print_usage ;;
     *) print_usage
        exit 1 ;;
@@ -30,7 +33,7 @@ if [${metadata_store} == '']; then
   mlflow server \
   --default-artifact-root "${artifact_store}" \
   --host 0.0.0.0 \
-  --port 5000
+  --port 5000 \
   exit 0
 fi
 
